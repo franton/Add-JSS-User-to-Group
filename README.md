0561dfbf1575c877de289ed63e56bb6a0e0a385c
Richard Purves <richardpurves@MacBook-Pro.local>
2017-08-18 13:34:52 +0200

    v1.0 initial release

---
+++ README.md
@@ -0,0 +1,16 @@
+Add-JSS-User-to-group
+=====================
+
+Takes the output from a .csv file in the following format:
+username1,usergroup1
+username1,usergroup2
+username2,usergroup1
+username2,usergroup2
+
+Then either adds those users to a new JSS user static group, or creates the group and adds the user.
+
+Nerd sniped by @mlgraham and @lewandowski on Mac Admins Slack. Hopefully this is of some use to them.
+
+Example datafile included in this repo, but shouldn't be used for anything other than testing.
+
+Please test on a non production environment!
---
+++ add-jss-user-to-group.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Attach user to static group from CSV file
+
+# Variables go here
+
+apiuser="admin"
+apipass="admin"
+csvloc="/path/to/testdata.csv"
+
+jssurl=$( /usr/bin/defaults read /Library/Preferences/com.jamfsoftware.jamf.plist jss_url )
+tmpxmlloc="/var/tmp"
+
+# Does the csv file exist?
+[ ! -f $csvloc ] && { echo "$csvloc file not found"; exit 99; }
+
+# Loop around CSV file. Reading record line by line.
+# Have to do a test to make sure we get the last line. Missing newline characters cause that fault.
+# I blame Excel for this.
+while IFS=, read -r user group || [ -n "$user" ]
+do
+	# Remove the carriage return, if it exists. Again I blame Excel for this hack.
+	username=$( echo $user | tr -d '\r' )
+	groupname=$( echo $group | tr -d '\r' )
+
+	# Does group xml file exist? It not, create!
+	if [ ! -f "$tmpxmlloc/${groupname}.xml" ];
+	then
+cat <<EOF > "${tmpxmlloc}/${groupname}.xml"
+<user_group>
+  <name>$groupname</name>
+  <is_smart>false</is_smart>
+  <is_notify_on_change>false</is_notify_on_change>
+  <users>
+EOF
+	fi
+
+	# Append to xml file record
+cat <<EOF >> "$tmpxmlloc/${groupname}.xml"
+    <user>
+      <username>$username</username>
+    </user>
+EOF
+
+# And loop around until done.
+done < "$csvloc"
+
+# Find all the xml files, append the ending tags
+find ${tmpxmlloc} -name '*.xml' | while IFS=$'\n' read file
+do
+
+	cat <<EOF >> "${file}"
+  </users>
+</user_group>
+EOF
+
+	# curl the file to the JSS API
+	curl -X POST -k -u ${apiuser}:${apipass} ${jssurl}JSSResource/usergroups/id/0 -d @$file -H "Content-Type:text/xml"
+
+	# Delete the temp file
+	rm ${file}.xml
+
+done
+
+# All done!
+echo "Upload complete"
